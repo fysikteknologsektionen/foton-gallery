e940286fa6ed02eafb47facd4c64dd02818a6ae6
Eric Carlsson <32720134+ECarlsson@users.noreply.github.com>
2021-06-07 14:20:29 +0200

    Validate env vars on start

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,2 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' })
+
+// Required env vars
+const requiredVars = ['APP_HOST', 'APP_PORT', 'APP_SECRET'];
+
+// Dict with verified env vars
+export const config: { [index: string]: string } = {};
+
+// Check if required env vars are set, else throw error
+requiredVars.map(variable => {
+  if (!process.env[variable])
+    throw new Error(`${variable} is not defined, add it to the .env file`);
+  else
+    config[variable] = process.env[variable]!;
+});
