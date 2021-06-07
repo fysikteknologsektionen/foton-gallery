2991d2de9b12ec066044ddcc4cc06ae1639cfcdb
Eric Carlsson <32720134+ECarlsson@users.noreply.github.com>
2021-06-07 14:21:32 +0200

    Model tweaks, add useful indicies

--- server/src/models/albumModel.ts
+++ server/src/models/albumModel.ts
@@ -2,33 +2,35 @@ import { model, Model, Schema } from 'mongoose';
 import { Album } from '../types';
 import slug from 'slug';
 
-const AlbumSchema: Schema<Album> = new Schema<Album>({
+const albumSchema: Schema<Album> = new Schema<Album>({
   name: { type: String, required: true },
-  slug: { type: String, required: true, unique: true },
+  slug: { type: String, required: true },
   description: String,
   images: [String],
   authors: [String],
-  dates: [Date],
+  date: {type: Date, required: true },
   thumbnail: { type: Schema.Types.ObjectId, ref: 'Album.images' }
 });
+
+albumSchema.index({ date: -1, slug: 1 }, { unique: true });
 
 /**
  * Generates a name slug and saves it alongside the name
  */
-AlbumSchema.pre('save', function(next) {
+albumSchema.pre('save', function(next) {
   const nameSlug: string = slug(this.name);
   this.slug = nameSlug;
   next();
 });
 
-AlbumSchema.post('updateOne', function(doc: Album, next) {
+albumSchema.post('updateOne', function(doc: Album, next) {
   /** TODO: remove image files when updating album to delete images */
   next();
 });
 
-AlbumSchema.post('remove', function(doc: Album, next) {
+albumSchema.post('remove', function(doc: Album, next) {
   /** TODO: remove image files when deleting album */
   next();
 });
 
-export const AlbumModel: Model<Album> = model('Album', AlbumSchema);
+export const AlbumModel: Model<Album> = model('Album', albumSchema);
--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -3,21 +3,24 @@ import { User } from '../types';
 import bcrypt from 'bcrypt';
 
 const userSchema: Schema<User> = new Schema<User>({
-  email: { type: String, required: true, lowercase: true, trim: true },
-  password: { type: String, required: true }
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, required: true }
 });
+
+userSchema.index({ email: 1 });
 
 /**
  * Hashes the password before saving it
  */
-userSchema.pre('save', async function (next) {
+ userSchema.pre('save', async function (next) {
   const hash: string = await bcrypt.hash(this.password, 12);
   this.password = hash;
   next();
 });
 
-userSchema.methods.comparePassword = function (password: string) {
-  return this.password === password;
+userSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
+  return await bcrypt.compare(password, this.password);
 };
 
 export const UserModel: Model<User> = model('User', userSchema);
