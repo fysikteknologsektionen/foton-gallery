e833278efd3761731aa1901b6999788c641a4582
Eric Carlsson <32720134+ECarlsson@users.noreply.github.com>
2021-06-28 16:53:10 +0200

    Use bootstrap icons instead of unicode

--- client/src/components/GoBack.tsx
+++ client/src/components/GoBack.tsx
@@ -9,6 +9,12 @@ export function GoBack({className}: {className?: string}) {
   const history = useHistory();
 
   return (
-    <button className={`${className} btn btn-sm btn-outline-secondary`} onClick={() => history.goBack()}>← Gå tillbaka</button>
+    <button
+      className={`${className} btn btn-sm btn-outline-secondary`}
+      onClick={() => history.goBack()}
+    >
+      <i className="bi-arrow-left"/>
+      &nbsp;Tillbaka
+    </button>
   );
 }
