161d65b1f4d25d7dafe89bd0bde8d4c40e689ec9
Eric Carlsson <32720134+ECarlsson@users.noreply.github.com>
2021-07-24 17:19:33 +0200

    Improve dev tools

--- server/src/dev/createDummyAlbums.ts
+++ server/src/dev/createDummyAlbums.ts
@@ -7,13 +7,12 @@ import fs from 'fs/promises';
 import path from 'path';
 import {processImages} from '../app/utils';
 
-const numAlbums = 30;
-const numImagesInAlbum = 6;
-
 /**
  * Creates dummy albums
+ * @param numAlbums Number of albums to create
+ * @param numImages Number of images in each album to create
  */
-async function createDummyAlbums() {
+async function createDummyAlbums(numAlbums: number, numImages: number) {
   console.log('Creating dummy albums (this may take a moment)...');
 
   const imagesDir = path.join(__dirname, 'images');
@@ -21,13 +20,13 @@ async function createDummyAlbums() {
   for (let i = 0; i < numAlbums; i++) {
     try {
       // Select 16 random elements from files
-      const images = faker.random.arrayElements(files, numImagesInAlbum);
+      const images = faker.random.arrayElements(files, numImages);
 
       // Copy files and rename them
       const newFileNames = [];
       for (const image of images) {
         const newFileName =
-                cryptoRandomString({length: 32}) + path.extname(image);
+          cryptoRandomString({length: 32}) + path.extname(image);
         await fs.copyFile(
             path.join(imagesDir, image),
             path.join(imagesDir, newFileName),
@@ -68,7 +67,17 @@ async function createDummyAlbums() {
 }
 
 if (require.main == module) {
-  createDummyAlbums();
+  const args = process.argv.slice(2);
+  const numAlbums = Number(args[0]);
+  const numImages = Number(args[1]);
+
+  if (!numAlbums || !numImages) {
+    console.error('Invalid arguments. Usage:');
+    console.error('> createDummyAlbums <# albums> <# images in each album>');
+    process.exit(1);
+  }
+
+  createDummyAlbums(numAlbums, numImages);
 } else {
   console.error('This file can only be run directly');
 }
--- server/src/dev/createUser.ts
+++ server/src/dev/createUser.ts
@@ -1,28 +1,23 @@
 import '../config';
 
-import {UserAuthenicationData, UserSession} from '../interfaces';
-
 import {User} from '../app/models';
 
 /**
  * Creates a new user
+ * @param username Username for new user
+ * @param password Password for new user
+ * @param role Role for new user
  */
-async function createUser() {
-  const user: UserAuthenicationData & UserSession = {
-    username: 'admin',
-    password: 'admin',
-    role: 'admin',
-  };
-
+async function createUser(username: string, password: string, role: string) {
   console.log('Creating a new user with:');
   console.log(
-      `Username: ${user.username}, 
-        Password: ${user.password}, 
-        Role: ${user.role}`,
+      `Username: ${username}, 
+        Password: ${password}, 
+        Role: ${role}`,
   );
 
   try {
-    const userDoc = new User({...user});
+    const userDoc = new User({username, password, role});
     await userDoc.save();
   } catch (error) {
     console.error();
@@ -32,7 +27,20 @@ async function createUser() {
 }
 
 if (require.main == module) {
-  createUser();
+  const args = process.argv.slice(2);
+  const username = args[0];
+  const password = args[1];
+  const role = args[2];
+
+  if (!username || !password || !role || !['user', 'admin'].includes(role)) {
+    console.error('Invalid arguments. Usage:');
+    console.error(
+        '> createUser <username> <password> <role ("user" or "admin")>',
+    );
+    process.exit(1);
+  }
+
+  createUser(username, password, role);
 } else {
   console.error('This file can only be run directly');
 }
