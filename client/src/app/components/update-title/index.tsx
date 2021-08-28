6bb20457ab52393435f2c0855609e92ab68934a7
Eric <32720134+ECarlsson@users.noreply.github.com>
2021-08-28 18:47:22 +0200

    Tweak updateTitle, partial fix for #17

--- client/src/app/components/update-title/index.tsx
+++ client/src/app/components/update-title/index.tsx
@@ -1,23 +1,20 @@
-import {useEffect} from 'react';
-import {useHistory} from 'react-router-dom';
+import {useLayoutEffect} from 'react';
+import {useLocation} from 'react-router-dom';
 
 /**
  * Component that registers a history listener for updating the page title
  * @returns null
  */
 export const UpdateTitle: React.VFC = () => {
-  const history = useHistory();
+  const {pathname} = useLocation();
 
-  useEffect(() => {
-    const unlisten = history.listen(() => {
-      setTimeout(() => {
-        document.title = `${
-          document.getElementsByTagName('h1').item(0)?.innerHTML ?? 'Galleri'
-        } | Foton`;
-      }, 100);
-    });
-    return unlisten;
-  }, [history]);
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      document.title = `${
+        document.getElementsByTagName('h1').item(0)?.innerHTML ?? 'Galleri'
+      } | Foton`;
+    }, 100);
+  }, [pathname]);
 
   return null;
 };
