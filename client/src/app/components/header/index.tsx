35c10d86db664d521e8611a99bc7e826184a9982
TheCoder5550 <alfons.nilsson@outlook.com>
2025-08-22 23:09:10 +0200

    Hardcoded socials links

--- client/src/app/components/header/index.tsx
+++ client/src/app/components/header/index.tsx
@@ -51,6 +51,36 @@ export const Header: React.VFC = () => {
               <img src={fotonLogo} alt="Foton" height="40" />
             </Link>
           </div>
+          <div
+            className="order-last d-flex gap-2 align-items-center me-3"
+          >
+            <span className="d-none d-lg-block">
+              Mer från oss:
+            </span>
+            <a
+              href="https://www.instagram.com/fotonchalmers/"
+              target="_blank"
+              rel="noreferrer"
+              type="button"
+              className="btn btn-light p-1"
+            >
+              <i className="bi bi-instagram " style={{
+                'background': `linear-gradient(45deg,rgba(115, 42, 155, 1) 0%, 
+                rgba(173, 76, 76, 1) 50%, rgba(255, 179, 0, 1) 100%)`,
+                'WebkitBackgroundClip': 'text',
+                'WebkitTextFillColor': 'transparent',
+              }}></i>
+            </a>
+            <a
+              href="https://www.facebook.com/FotonChalmers"
+              target="_blank"
+              rel="noreferrer"
+              type="button"
+              className="btn btn-light p-1"
+            >
+              <i className="bi bi-facebook" style={{color: '#0d6efd'}}></i>
+            </a>
+          </div>
           {session && (
             <div className="d-flex me-3 me-md-0 order-md-last">
               <span className="d-none d-sm-block navbar-text me-2">
@@ -65,7 +95,7 @@ export const Header: React.VFC = () => {
             </div>
           )}
           <button
-            className="navbar-toggler"
+            className="navbar-toggler order-last"
             type="button"
             aria-controls="navbar-nav-content"
             aria-expanded={navCollapsed ? 'false' : 'true'}
