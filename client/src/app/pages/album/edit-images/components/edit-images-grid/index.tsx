bcb7f6f7ff60974a259eb6775b034881238378e7
TheCoder5550 <alfons.nilsson@outlook.com>
2025-08-22 19:01:00 +0200

    Make images draggable when editing album

--- client/src/app/pages/album/edit-images/components/edit-images-grid/index.tsx
+++ client/src/app/pages/album/edit-images/components/edit-images-grid/index.tsx
@@ -1,14 +1,26 @@
 import 'react-image-lightbox/style.css';
 import '../../../../../styles/lightbox-override.scss';
 
-import {Field, FieldArray, Form, Formik} from 'formik';
+import {Album, Image} from '../../../../../../interfaces';
+import {
+  DndContext,
+  PointerSensor,
+  closestCenter,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {Field, FieldArray, FieldArrayRenderProps, Form, Formik} from 'formik';
 import React, {useContext, useState} from 'react';
-
-import {Album} from '../../../../../../interfaces';
+import {
+  SortableContext,
+  rectSortingStrategy,
+} from '@dnd-kit/sortable';
+import {CSS} from '@dnd-kit/utilities';
 import ImageFullscreenView from '../../../components/image-fullscreen-view';
 import {Thumbnail} from '../../../../../components/common/thumbnail';
 import axios from 'axios';
 import {toastContext} from '../../../../../contexts/toast';
+import {useSortable} from '@dnd-kit/sortable';
 
 /**
  * Component for rendering a grid of an album's images that allows editing
@@ -17,6 +29,14 @@ import {toastContext} from '../../../../../contexts/toast';
  * @return Edit images grid component
  */
 export const EditImagesGrid: React.VFC<Album> = (album) => {
+  const sensors = useSensors(
+      useSensor(PointerSensor, {
+        activationConstraint: {
+          distance: 10,
+        },
+      }),
+  );
+
   const newToast = useContext(toastContext);
   const [activeImage, setActiveImage] = useState(0);
   const [showLightbox, setShowLightbox] = useState(false);
@@ -66,101 +86,51 @@ export const EditImagesGrid: React.VFC<Album> = (album) => {
             )}
             <FieldArray name="images">
               {(arrayHelpers) => (
-                <div
-                  className={`d-grid gap-3 justify-content-center
-                    align-items-center`}
-                  style={{
-                    gridTemplateColumns:
-                      'repeat(auto-fit, minmax(180px, max-content))',
+                <DndContext
+                  sensors={sensors}
+                  collisionDetection={closestCenter}
+                  onDragEnd={(event) => {
+                    const {active, over} = event;
+
+                    if (over && active.id !== over.id) {
+                      const oldIndex =
+                        values.images.indexOf(active.id as string);
+                      const newIndex =
+                        values.images.indexOf(over.id as string);
+
+                      arrayHelpers.move(oldIndex, newIndex);
+                    }
                   }}
                 >
-                  {values.images.map((image, index) => (
-                    <div key={image}>
-                      <div
-                        className="position-relative rounded"
-                        style={{
-                          boxShadow:
-                            image === values.thumbnail ?
-                              '0 0 0 0.50rem var(--bs-teal)' :
-                              'none',
-                          transition: 'box-shadow 0.15s ease-in-out',
-                          cursor: 'zoom-in',
-                        }}
-                        onClick={(e) => {
-                          setShowLightbox(true);
-                          setActiveImage(images
-                              .findIndex((element) => element._id === image),
-                          );
-                          e.preventDefault();
-                        }}
-                      >
-                        <Field
-                          className="visually-hidden"
-                          name={`images.${index}`}
+                  <SortableContext
+                    items={values.images}
+                    strategy={rectSortingStrategy}
+                  >
+                    <div
+                      className={`d-grid gap-3 justify-content-center
+                        align-items-center`}
+                      style={{
+                        gridTemplateColumns:
+                          'repeat(auto-fit, minmax(180px, max-content))',
+                      }}
+                    >
+                      {values.images.map((image, index) => (
+                        <SortableImage
+                          key={image}
+                          id={image}
+                          image={image}
+                          index={index}
+                          images={images}
+                          values={values}
+                          arrayHelpers={arrayHelpers}
+                          setFieldValue={setFieldValue}
+                          setActiveImage={setActiveImage}
+                          setShowLightbox={setShowLightbox}
                         />
-                        <Thumbnail
-                          filename={
-                            images.find((element) => element._id === image)
-                                ?.filename
-                          }
-                          alt="Albumbild"
-                        />
-                        <button
-                          className="position-absolute btn btn-secondary"
-                          style={{left: 0, top: '50%'}}
-                          type="button"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            arrayHelpers.move(index, index - 1);
-                          }}
-                          aria-label="Flytta bakåt"
-                        >
-                          <i className="bi-arrow-left" aria-hidden="true" />
-                        </button>
-                        <button
-                          className="position-absolute btn btn-secondary"
-                          style={{right: 0, top: '50%'}}
-                          type="button"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            arrayHelpers.move(index, index + 1);
-                          }}
-                          aria-label="Flytta framåt"
-                        >
-                          <i className="bi-arrow-right" aria-hidden="true" />
-                        </button>
-                        <button
-                          className="position-absolute btn btn-danger"
-                          style={{right: 0, top: 0}}
-                          type="button"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            arrayHelpers.remove(index);
-                          }}
-                          aria-label="Ta bort"
-                        >
-                          <i className="bi-x" aria-hidden="true" />
-                        </button>
-                        <button
-                          className="position-absolute btn btn-success"
-                          style={{left: 0, top: 0}}
-                          type="button"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            setFieldValue('thumbnail', image);
-                          }}
-                          aria-label="Markera som omslagsbild"
-                        >
-                          {values.thumbnail === image ? (
-                            <i className="bi-star-fill" aria-hidden="true" />
-                          ) : (
-                            <i className="bi-star" aria-hidden="true" />
-                          )}
-                        </button>
-                      </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
+                  </SortableContext>
+                </DndContext>
               )}
             </FieldArray>
             <button className="btn btn-primary mt-3" type="submit">
@@ -178,5 +148,173 @@ export const EditImagesGrid: React.VFC<Album> = (album) => {
         album={album}
       />
     </>
+  );
+};
+
+const SortableImage: React.VFC<{
+  id: string;
+  image: string;
+  index: number;
+  images: Image[];
+  values: {
+    images: string[];
+    thumbnail?: string | undefined;
+  };
+  arrayHelpers: FieldArrayRenderProps;
+  setFieldValue: (
+    field: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    value: any,
+    shouldValidate?: boolean | undefined
+  ) => void;
+  setActiveImage: React.Dispatch<React.SetStateAction<number>>;
+  setShowLightbox: React.Dispatch<React.SetStateAction<boolean>>;
+}> = ({
+  id,
+  image,
+  index,
+  images,
+  values,
+  arrayHelpers,
+  setFieldValue,
+  setActiveImage,
+  setShowLightbox,
+}) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({id: id});
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    height: '100%',
+    zIndex: isDragging ? 100 : undefined,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <div
+        className="position-relative rounded"
+        style={{
+          boxShadow:
+            image === values.thumbnail ?
+              '0 0 0 0.50rem var(--bs-teal)' :
+              'none',
+          transition: 'box-shadow 0.15s ease-in-out',
+          cursor: 'move',
+        }}
+      >
+        <Field
+          className="visually-hidden"
+          name={`images.${index}`}
+        />
+        <Thumbnail
+          filename={
+            images.find((element) => element._id === image)?.filename
+          }
+          alt="Albumbild"
+        />
+        <div
+          className="position-absolute d-flex end-0 top-0 gap-1 p-1"
+          // style={{right: 0, top: 0}}
+        >
+          <button
+            className="btn btn-secondary"
+            type="button"
+            onClick={(e) => {
+              setShowLightbox(true);
+              setActiveImage(images
+                  .findIndex(
+                      (element) => element._id === image,
+                  ),
+              );
+              e.preventDefault();
+            }}
+            aria-label="Visa i fullskärm"
+            title="Visa i fullskärm"
+          >
+            <i className="bi-arrows-fullscreen" aria-hidden="true" />
+          </button>
+          <button
+            className="btn btn-success"
+            type="button"
+            onClick={(e) => {
+              e.stopPropagation();
+              setFieldValue('thumbnail', image);
+            }}
+            aria-label="Markera som omslagsbild"
+            title="Markera som omslagsbild"
+          >
+            {values.thumbnail === image ? (
+              <i className="bi-star-fill" aria-hidden="true" />
+            ) : (
+              <i className="bi-star" aria-hidden="true" />
+            )}
+          </button>
+          <button
+            className="btn btn-danger"
+            type="button"
+            onClick={(e) => {
+              e.stopPropagation();
+              arrayHelpers.remove(index);
+            }}
+            aria-label="Ta bort"
+            title="Ta bort"
+          >
+            <i className="bi-x" aria-hidden="true" />
+          </button>
+        </div>
+        {/* <button
+          className="position-absolute btn btn-danger"
+          style={{right: 0, top: 0}}
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            arrayHelpers.remove(index);
+          }}
+          aria-label="Ta bort"
+        >
+          <i className="bi-x" aria-hidden="true" />
+        </button>
+        <button
+          className="position-absolute btn btn-success"
+          style={{left: 0, top: 0}}
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            setFieldValue('thumbnail', image);
+          }}
+          aria-label="Markera som omslagsbild"
+        >
+          {values.thumbnail === image ? (
+            <i className="bi-star-fill" aria-hidden="true" />
+          ) : (
+            <i className="bi-star" aria-hidden="true" />
+          )}
+        </button>
+        <button
+          className="position-absolute btn btn-secondary"
+          style={{right: 0, bottom: 0}}
+          type="button"
+          onClick={(e) => {
+            setShowLightbox(true);
+            setActiveImage(images
+                .findIndex(
+                    (element) => element._id === image,
+                ),
+            );
+            e.preventDefault();
+          }}
+          aria-label="Visa i fullskärm"
+        >
+          <i className="bi-arrows-fullscreen" aria-hidden="true" />
+        </button> */}
+      </div>
+    </div>
   );
 };
